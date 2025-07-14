c34a169d1a3123385e3f77907eae29886a013ed5
moncef1818 <monomoncef7@gmail.com>
2025-07-14 20:10:32 +0100

    Added: add some methods to the products DAO class

--- src/ProductDAO.java
+++ src/ProductDAO.java
@@ -9,7 +9,7 @@ public class ProductDAO {
         String sql = "INSERT INTO products (name , category, description,stock,price) VALUES (?,?,?,?,?)";
 
         try (Connection conn = DBConnection.getConnection()){
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            PreparedStatement preparedStatement = conn.prepareStatement(sql,PreparedStatement.RETURN_GENERATED_KEYS);
             preparedStatement.setString(1,product.getProductName());
             preparedStatement.setString(2, product.getCategory());
             preparedStatement.setString(3,product.getDescription());
@@ -41,21 +41,101 @@ public class ProductDAO {
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while(resultSet.next()){
-                Product product = new Product(
-                        resultSet.getString("name"),
-                        resultSet.getString("category"),
-                        resultSet.getInt("id"),
-                        resultSet.getString("description"),
-                        resultSet.getInt("stock"),
-                        resultSet.getDouble("price")
-                );
-
-                products.add(product);
+            while (resultSet.next()){
+                products.add(buildProduct(resultSet));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         return products;
     }
+
+    public Product searchProductByName(String name){
+        String sql = "SELECT * FROM products WHERE name = ?";
+
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,name);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return buildProduct(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    public ArrayList<Product> getProductsByCategory(String category) {
+        ArrayList<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM products WHERE category = ?";
+
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,category);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                products.add(buildProduct(resultSet));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return products;
+    }
+    public ArrayList<Product> getProductsByNameLike(String nameLike){
+        ArrayList<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM products WHERE name LIKE ?";
+
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1,"%" + nameLike + "%");
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                products.add(buildProduct(resultSet));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return products;
+    }
+    public ArrayList<Product> getProductsInStock(){
+        ArrayList<Product> products = new ArrayList<>();
+        String sql = "SELECT * FROM products WHERE stock > 0 ";
+
+        try (Connection conn = DBConnection.getConnection()){
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                products.add(buildProduct(resultSet));
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return products;
+    }
+
+    private static Product buildProduct(ResultSet rs) throws SQLException {
+        return new Product(
+                rs.getString("name"),
+                rs.getString("category"),
+                rs.getInt("id"),
+                rs.getString("description"),
+                rs.getInt("stock"),
+                rs.getDouble("price")
+        );
+    }
+
 }
